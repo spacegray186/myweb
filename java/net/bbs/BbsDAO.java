1f64abf797bdf562f43e129c60528b23823f00a3
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-10-13 20:00:40 +0900

    Day053_221013

--- java/net/bbs/BbsDAO.java
+++ java/net/bbs/BbsDAO.java
@@ -185,6 +185,167 @@ public class BbsDAO { //데이터베이스 관련 작업
             DBClose.close(con, pstmt);
         }//end
         return cnt;
-    }//delete() end    
-        
+    }//delete() end
+    
+    public int updateproc(BbsDTO dto) {
+    	int cnt=0;
+    	try {
+    		con=dbopen.getConnection();
+    		
+    		sql=new StringBuilder();
+    		sql.append(" UPDATE tb_bbs ");
+    		sql.append(" SET wname=? ");
+    		sql.append(" , subject=? ");
+    		sql.append(" , content=? ");
+    		sql.append(" , ip=? ");
+    		sql.append(" WHERE bbsno=? AND passwd=? ");
+    		
+    		pstmt=con.prepareStatement(sql.toString());
+    		pstmt.setString(1, dto.getWname());
+    		pstmt.setString(2, dto.getSubject());
+    		pstmt.setString(3, dto.getContent());
+    		pstmt.setString(4, dto.getIp());
+    		pstmt.setInt(5, dto.getBbsno());
+    		pstmt.setString(6, dto.getPasswd());
+    		
+    		cnt=pstmt.executeUpdate();
+    		
+    	}catch (Exception e) {
+    		System.out.println("수정 실패 : " + e);
+    	}finally {
+    		DBClose.close(con, pstmt);
+    	}//end
+    	return cnt;
+    }//updateproc() end
+    
+    public int reply(BbsDTO dto) {
+        int cnt=0;
+        try {
+            con=dbopen.getConnection();
+            sql=new StringBuilder();
+            
+            //1)부모글 정보 가져오기(select문)
+            int grpno=0;   //부모글 그룹번호
+            int indent=0;  //부모글 들여쓰기
+            int ansnum=0;  //부모글 글순서
+            sql.append(" SELECT grpno, indent, ansnum ");
+            sql.append(" FROM tb_bbs ");
+            sql.append(" WHERE bbsno=? ");
+            pstmt=con.prepareStatement(sql.toString());
+            pstmt.setInt(1, dto.getBbsno());
+            rs=pstmt.executeQuery();
+            if(rs.next()) {
+                //그룹번호 : 부모글 그룹번호 그대로 가져오기
+                grpno=rs.getInt("grpno");
+                //들여쓰기 : 부모글 들여쓰기 + 1
+                indent=rs.getInt("indent")+1;
+                //글순서   : 부모글 글순서 +1
+                ansnum=rs.getInt("ansnum")+1;
+            }//if end
+            
+            
+            //2)글순서 재조정하기(update문)
+            sql.delete(0, sql.length()); //1)단계에서 사용했던 sql값 지우기
+            sql.append(" UPDATE tb_bbs ");
+            sql.append(" SET ansnum=ansnum+ 1 ");
+            sql.append(" WHERE grpno=? AND ansnum>=? ");
+            pstmt=con.prepareStatement(sql.toString());
+            pstmt.setInt(1, grpno);
+            pstmt.setInt(2, ansnum);
+            pstmt.executeUpdate();            
+            
+            
+            //3)답변글 추가하기(insert문)
+            sql.delete(0, sql.length());
+            sql.append(" INSERT INTO tb_bbs(bbsno, wname, subject, content, passwd, ip, grpno, indent, ansnum) ");
+            sql.append(" VALUES(bbs_seq.nextval,?, ?, ?, ?, ?, ?, ?, ?) ");
+            
+            pstmt=con.prepareStatement(sql.toString());
+            pstmt.setString(1, dto.getWname());
+            pstmt.setString(2, dto.getSubject());
+            pstmt.setString(3, dto.getContent());
+            pstmt.setString(4, dto.getPasswd());
+            pstmt.setString(5, dto.getIp());
+            pstmt.setInt(6, grpno);  //1)단계에서 만든 그룹번호
+            pstmt.setInt(7, indent); //1)단계에서 만든 들여쓰기
+            pstmt.setInt(8, ansnum); //1)단계에서 만들 글순서
+            
+            cnt=pstmt.executeUpdate();
+            
+        }catch (Exception e) {
+            System.out.println("답변쓰기 실패:"+e);
+        }finally {
+            DBClose.close(con, pstmt,rs);
+        }//end
+        return cnt;
+    }//reply() end
+    
+    public int count2(String col, String word) {
+        int cnt=0;
+        try {
+            con=dbopen.getConnection();
+            
+            sql=new StringBuilder();
+            sql.append(" SELECT COUNT(*) as cnt ");
+            sql.append(" FROM tb_bbs ");
+            
+            if(word.length()>=1) { //검색어가 존재한다면
+                String search="";
+                if(col.equals("subject_content")) {
+                    search += " WHERE subject LIKE '%" + word + "%' ";
+                    search += " OR    content LIKE '%" + word + "%' ";
+                }else if(col.equals("subject")) {
+                    search += " WHERE subject LIKE '%" + word + "%' ";
+                }else if(col.equals("content")) {
+                    search += " WHERE content LIKE '%" + word + "%' ";
+                }else if(col.equals("wname")) {
+                    search += " WHERE wname LIKE '%" + word + "%' ";
+                }//if end 
+                sql.append(search);
+            }//if end
+            
+            pstmt=con.prepareStatement(sql.toString());
+            rs=pstmt.executeQuery();
+            if(rs.next()) {
+                cnt=rs.getInt("cnt");
+            }//if end
+            
+        }catch (Exception e) {
+            System.out.println("검색 글갯수 실패:"+e);
+        }finally {
+            DBClose.close(con, pstmt, rs);
+        }//end
+        return cnt;
+    }//count2() end
+    
+    public int replyCnt(BbsDTO dto) {
+    	int cnt=0;
+        try {
+            con=dbopen.getConnection();//DB연결
+            sql=new StringBuilder();
+            int grpno=rs.getInt("grpno");
+            int indent=rs.getInt("indent");
+            
+            sql.append(" SELECT COUNT(*) as cnt ");
+            sql.append(" FROM tb_bbs ");
+            sql.append(" WHERE grpno=? and indent >0 ");
+
+            pstmt=con.prepareStatement(sql.toString());
+            pstmt.setInt(1, dto.getGrpno());
+            
+            rs=pstmt.executeQuery();
+            if(rs.next()) {
+            	if(grpno==dto.getGrpno()&&indent>0) {
+            		cnt=rs.getInt("cnt");
+            	}
+            }//if end
+            
+        }catch (Exception e) {
+            System.out.println("답변 개수 세기 실패 : " + e);
+        }finally {
+            DBClose.close(con, pstmt, rs);
+        }//end
+        return cnt;
+    }
+    
 }//class end
