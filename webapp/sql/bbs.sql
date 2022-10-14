5c6c8e17dac773a043131806d09fbb835f4e05e9
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-10-14 18:35:10 +0900

    Day054_221014

--- webapp/sql/bbs.sql
+++ webapp/sql/bbs.sql
@@ -78,7 +78,154 @@ where bbsno=? and passwd=?
 --조회수 증가
 update tb_bbs set readcnt=readcnt+1 
 where bbsno=?
+///////////////////////////////////////////////////////////
+
+
+[답변쓰기 알고리즘]
+
+- 새글 쓰기 : 최초의 부모글
+- 답변 쓰기 : 자식글
+
+- 그룹번호(grpno)  : 부모글 그룹번호와 동일하게
+- 들여쓰기(indent) : 부모글 들여쓰기 + 1
+- 글순서  (ansnum) : 부모글 글순서 + 1 한 후, 글순서 재조정
+
+
+번호  제목            그룹번호 들여쓰기 글순서
+1    제주도            1      0      0
+2    서울시            2      0      0
+     ▶종로구           2      1      1
+     ▶▶관철동          2      2      2
+     ▶▶▶보신각         2      3      3
+     ▶▶인사동          2      2      4
+     ▶강남구           2      1      5
+     ▶▶도곡동          2      2      6
+     ▶▶역삼동          2      2      6->7
+     ▶마포구           2      1      7->8
+3    부산시            3      0      0
+
+
+- 글순서(ansnum) 재조정
+update tb_bbs
+set ansnum=ansnum+1
+where grpno=2 and ansnum>=6
+///////////////////////////////////////////////////////////
+
+
+[검색]
+-- 제목+내용에서 '파스타'가 있는지 검색
+where subject like '%파스타%' or content like '%파스타%'
+
+-- 제목에서 '파스타'가 있는지 검색
+where subject like '%파스타%'
+
+-- 내용에서 '파스타'가 있는지 검색
+where content like '%파스타%'
+
+-- 작성자에서 '파스타'가 있는지 검색
+where wname like '%파스타%'
+
+/////////////////////////////////////////////////////
+
+[과제] 제목과 댓글(자식글)의 개수를 조회하시오 (indent칼럼명 참조)
+
+      제목         답변갯수
+     ---------  --------
+      무궁화
+      코리아         2
+      점심시간       3
+      한글날
+      오필승
+      
+select subject, grpno, indent from tb_bbs order by grpno desc;
+
+select grpno, count(*)
+from tb_bbs
+group by grpno;
+
+select BB.subjeck, AA.reply
+from(
+		select grpno, count(*)-1 as reply
+		from tb_bbs
+		group by grpno
+	) AA join tb_bbs BB
+on AA.grpno=BB.grpno
+where BB.indent=0
+order by BB.grpno desc;
 
 
 
+--출력 줄수
+set pagesize 100;
+--한줄 출력 글자개수
+set linesize 100;
+--칼럼길이 10칸 임시 조정
+col wname for a10;
+col subject for a20;
 
+
+[페이징]
+- rownum 줄번호 활용
+
+1)
+SELECT bbsno, subject, wname, readcnt, indent, regdt
+FROM tb_bbs
+ORDER BY grpno DESC, ansnum ASC;
+
+2) rownum추가 - 줄번호까지 정렬됨
+SELECT bbsno, subject, wname, readcnt, indent, regdt, rownum
+FROM tb_bbs
+ORDER BY grpno DESC, ansnum ASC;
+
+3) 1)의 SQL문을 셀프조인하고, rownum 추가
+SELECT bbsno, subject, wname, readcnt, indent, regdt, rownum
+from (
+		SELECT bbsno, subject, wname, readcnt, indent, regdt
+		FROM tb_bbs
+		ORDER BY grpno DESC, ansnum ASC
+	);
+
+4) 줄번호 1~5 조회 (1페이지)
+SELECT bbsno, subject, wname, readcnt, indent, regdt, rownum
+FROM (
+		SELECT bbsno, subject, wname, readcnt, indent, regdt
+		FROM tb_bbs
+		ORDER BY grpno DESC, ansnum ASC
+	)
+WHERE rownum>=1 AND rownum<=5;
+
+5) 줄번호 6~10 조회(2페이지) -> 조회안됨. 선택된 레코드가 없습니다
+SELECT bbsno, subject, wname, readcnt, indent, regdt, rownum
+FROM (
+		SELECT bbsno, subject, wname, readcnt, indent, regdt
+		FROM tb_bbs
+		ORDER BY grpno DESC, ansnum ASC
+	)
+WHERE rownum>=6 AND rownum<=10;
+
+6) 줄번호가 있는 3)의 테이블을 한번 더 셀프조인하고, rownum 칼럼명을 r을 바꾼다
+SELECT *
+FROM (
+	SELECT bbsno, subject, wname, readcnt, indent, regdt, rownum as r
+	from (
+			SELECT bbsno, subject, wname, readcnt, indent, regdt
+			FROM tb_bbs
+			ORDER BY grpno DESC, ansnum ASC
+		)
+	)
+WHERE r>=6 AND r<=10;
+
+7) 페이징+검색
+	예) 제목에서 '파스타'가 있는 행을 검색해서 2페이지(6행~10행) 조회하시오
+SELECT *
+FROM (
+		SELECT bbsno, subject, wname, readcnt, indent, regdt, rownum as r
+		from (
+				SELECT bbsno, subject, wname, readcnt, indent, regdt
+				FROM tb_bbs
+				WHERE subject LIKE '%파스타%'
+				ORDER BY grpno DESC, ansnum ASC
+			)
+		)
+WHERE r>=6 AND r<=10;
+	
