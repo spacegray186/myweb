a89a8c5ce255538364ac53a3bb86fd9a402cd2f7
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-10-19 18:27:18 +0900

    Day057_221019

--- java/net/member/MemberDAO.java
+++ java/net/member/MemberDAO.java
@@ -1,9 +1,12 @@
 package net.member;
 
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Date;
 
+import net.bbs.BbsDTO;
 import net.utility.DBClose;
 import net.utility.DBOpen;
 
@@ -101,6 +104,107 @@ public class MemberDAO { //Data Access Object
     	}//end
     	return cnt;
     }//duplecateEmail() end
-        
+    
+    public int create(MemberDTO dto) {
+        int cnt=0;
+        try {
+            con=dbopen.getConnection();
+            
+            sql=new StringBuilder();
+            sql.append(" INSERT INTO member(id, passwd, mname, tel, email, zipcode, address1, address2, job, mlevel, mdate) ");
+            sql.append(" VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, 'D1', sysdate) ");
+            
+            pstmt=con.prepareStatement(sql.toString());
+            pstmt.setString(1, dto.getId());
+            pstmt.setString(2, dto.getPasswd());
+            pstmt.setString(3, dto.getMname());
+            pstmt.setString(4, dto.getTel());
+            pstmt.setString(5, dto.getEmail());
+            pstmt.setString(6, dto.getZipcode());
+            pstmt.setString(7, dto.getAddress1());
+            pstmt.setString(8, dto.getAddress2());
+            pstmt.setString(9, dto.getJob());
+            
+            cnt=pstmt.executeUpdate();
+            
+        }catch (Exception e) {
+            System.out.println("회원 가입 실패 : " + e);
+        }finally {
+            DBClose.close(con, pstmt);
+        }//end
+        return cnt;
+    }//create() end
+    
+    public String findID(String mname, String email) {
+    	String id=null;
+        try {
+            con=dbopen.getConnection();
+            
+            sql=new StringBuilder();
+            sql.append(" SELECT id ");
+            sql.append(" FROM member ");
+            sql.append(" WHERE mname=? AND email=? ");
+            
+            pstmt=con.prepareStatement(sql.toString());
+            pstmt.setString(1, mname);
+            pstmt.setString(2, email);
+            
+            rs= pstmt.executeQuery();            
+            if(rs.next()) {
+                id=rs.getString("id");
+            }//if end
+            
+        }catch (Exception e) {
+            System.out.println("아이디/비번 찾기 실패 : " + e);
+        }finally {
+            DBClose.close(con, pstmt, rs);
+        }//end
+        return id;
+    }//findID() end
+    
+    public String tempPW(int size) {
+        char[] charSet = new char[] {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '!', '@', '#', '$', '%', '^', '&' };
+
+        StringBuffer sb = new StringBuffer();
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(new Date().getTime());
+
+        int idx = 0;
+        int len = charSet.length;
+        for (int i=0; i<size; i++) {
+            // idx = (int) (len * Math.random());
+            idx = sr.nextInt(len);    // 강력한 난수를 발생시키기 위해 SecureRandom을 사용한다.
+            sb.append(charSet[idx]);
+        }
+        return sb.toString();
+    }//getRamdomPassword() end
+    
+    public int updatePW(MemberDTO dto) {
+    	int cnt=0;
+    	try {
+    		con=dbopen.getConnection();
+    		
+    		sql=new StringBuilder();
+    		sql.append(" UPDATE member ");
+    		sql.append(" SET passwd=? ");
+    		sql.append(" WHERE id=? ");
+    		
+    		pstmt=con.prepareStatement(sql.toString());
+    		pstmt.setString(1, dto.getPasswd());
+    		pstmt.setString(2, dto.getId());
+    		
+    		cnt=pstmt.executeUpdate();
+    		
+    	}catch (Exception e) {
+    		System.out.println("임시 비밀번호 수정 실패 : " + e);
+    	}finally {
+    		DBClose.close(con, pstmt);
+    	}//end
+    	return cnt;
+    }//updatePW() end
     
 }//class end
